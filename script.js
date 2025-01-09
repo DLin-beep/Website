2eda99957dfc99e2172a6ada1b1e1b8a4c3f6e3e
DLin-beep <120617220+DLin-beep@users.noreply.github.com>
2025-01-09 18:42:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+function toggleActive(activeSection) {
+    const sections = document.querySelectorAll('.section');
+    const isActive = activeSection.classList.contains('active');
 
+    sections.forEach((section) => {
+        section.classList.remove('active', 'inactive');
+    });
+
+    if (!isActive) {
+        activeSection.classList.add('active');
+        sections.forEach((section) => {
+            if (!section.classList.contains('active')) {
+                section.classList.add('inactive');
+            }
+        });
+    }
+}
+
+// Prevent clicks on all content containers from propagating
+document.querySelectorAll('.content, .content-container, .projects-container, .contact-container').forEach(container => {
+    container.addEventListener('click', event => {
+        event.stopPropagation();
+    });
+});
